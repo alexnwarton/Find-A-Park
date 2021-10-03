8dbc62b252e637d1936d26468f0e3cb4e3dad5ad
Alex Warton <alexnwarton@gmail.com>
2021-10-03 16:24:43 -0700

    added calcDistance and revised nearbyParks functions

--- script.js
+++ script.js
@@ -1,25 +1,53 @@
 const baseUrl = "https://developer.nps.gov/api/v1/parks";
 const apiKey = "6XXFeplxHFNsohc79vCep4HHyd10N4ARh557F2ep"
 
+
+
+
+
+
 let btn = document.querySelector("button");
 btn.addEventListener("click", () => {
 	let userInput = document.querySelector("input").value;
+	let zipToLatLong = [];
 
-	// fetching data from API based on park postal code
-	fetch(`${baseUrl}?limit=500&q="postalCode"&&api_key=${apiKey}`)
-		
-		.then((response) => {
-			return response.json();
+	// Converts the zipcode input by user to latitude and longitude values using mapquest API
+	fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=7eswptpG7eFsKM6dEKesj010foCEAYFQ&location=${userInput}`)
+		.then((convertResponse) => {
+			return convertResponse.json();
 		})
 		.then((jsonResponse) => {
 			
-			let parkList = findParks(jsonResponse);
-			console.log(nearbyParks(parkList, userInput));
+			zipToLatLong.push(jsonResponse.results[0].locations[0].latLng.lat);
+			zipToLatLong.push(jsonResponse.results[0].locations[0].latLng.lng);
+			userInput = zipToLatLong;	
+
+			// fetching data from national parks API based on park postal code
+			fetch(`${baseUrl}?limit=500&q="postalCode"&&api_key=${apiKey}`)
+		
+				.then((response) => {
+				return response.json();
+				})
+				.then((jsonResponse) => {
 			
+					let parkList = findParks(jsonResponse);
+					//console.log(nearbyParks(parkList, userInput));
+					console.log(parkList);
+					console.log(userInput);
+					console.log(nearbyParks(parkList, userInput));
+			
+				})
+				
 		})
+			
+	
+
+	
+	
 
 })
 
+// Searching through the list of all national sites to return national parks
 const findParks = (siteInfo) => {
 	const natParks = [];
 			//const natMonuments = [];
@@ -32,13 +60,44 @@ const findParks = (siteInfo) => {
 	return natParks;
 }
 
+
+// calcDistance function referenced from https://www.movable-type.co.uk/scripts/latlong.html to calculate the distance between two points based on given latitude and longitute values
+const calcDistance = (park1, park2) => {
+	const radius = 6371; // radius of the earth in kilometers
+	let latDiffDegrees = (park2[0] - park1[0]) * (Math.PI / 180);
+	let longDiffDegrees = (park2[1] - park1[1]) * (Math.PI / 180);
+	
+	let a = Math.sin(latDiffDegrees / 2) * Math.sin(latDiffDegrees / 2) +
+    Math.cos((park1[0]) * (Math.PI / 180)) * Math.cos((park2[0]) * (Math.PI / 180)) * 
+    Math.sin(longDiffDegrees / 2) * Math.sin(longDiffDegrees / 2); 
+
+
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+	let distance = radius * c;
+	//convert distance from meters to miles
+	distance = distance * 0.621371;
+	return distance;
+
+}
+
+//Finds the closest park based on the user input
 const nearbyParks = (parkList, input) => {
- const natParksNear = [];
- for(let i = 0; i < parkList.length; i++){
- 	let postalCodes = parkList[i].addresses[0].postalCode;
- 	if(input[0] === postalCodes[0]){
- 		natParksNear.push(parkList[i]);		
+ let natParksNear = parkList[0];
+ let minDistance = calcDistance(input, [parseFloat(parkList[0].latitude), parseFloat(parkList[0].longitude)]);
+ let parkLat;
+ let parkLong;
+ let distance;
+ console.log("Iterate through parks: ");
+ parkList.forEach((park, i) => {
+ 	parkLat = parseFloat(park.latitude);
+ 	parkLong = parseFloat(park.longitude);
+ 	distance = calcDistance(input, [parkLat, parkLong]);
+ 	console.log(park.name, distance);
+ 	if(distance < minDistance) {
+ 		minDistance = distance;
+ 		natParksNear = park;
  	}
- }
+ })
+ 	
  	return natParksNear;
 }
